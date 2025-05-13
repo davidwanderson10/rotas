dd4896eaf4c4f11531986fdf0c308accc68d850e
DAVID WANDERSON <profdavidwanderson@gmail.com>
2025-05-13 20:04:39 -0300

    criando a estrutura de layout vite

---
+++ aula-rotas/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Aula: Rotas</p>
+        <button>Home</button>
+      </div>
+    </>
+  )
+}
+
+export default App
